9133e1242968e0b2de1697720359acc44185673f
jasonzhao <zzs3033@163.com>
2018-11-14 15:02:45 +0800

    keep change

--- main.py
+++ main.py
@@ -98,16 +98,16 @@ if __name__=="__main__":
         images, id_labels, pose_labels, Nd, Np, Nz, channel_num = create_randomdata()
     else:
         print('n\Loading data from [%s]...' % args.data_place)
-        try:
-            Nd, Np, Nz, channel_num = DataLoader()
-            pose_dict = {'110': 1, '120': 2, '090': 3, '080': 4, '130': 5, '140': 6, '051': 7, '050': 8, '041': 9,
+
+        Nd, Np, Nz, channel_num = DataLoader()
+        pose_dict = {'110': 1, '120': 2, '090': 3, '080': 4, '130': 5, '140': 6, '051': 7, '050': 8, '041': 9,
                          '190': 10, '200': 11, '010': 12, '240': 13}
-            dataloader = get_loader(image_dir='/home/manager/jason/DR-GAN-11/data/Session01', Np=13, Nd=200,
+        dataloader = get_loader(image_dir='/home/manager/jason/DR-GAN-11/data/Session01', Np=13, Nd=200,
                                     pose_dict=pose_dict, image_size=110, batch_size=args.batch_size, mode='train',
                                     num_workers=1)
         # print('test')
-        except:
-            print("Sorry, failed to load data")
+        # except:
+            # print("Sorry, failed to load data")
 
     # model
     if args.snapshot is None:
