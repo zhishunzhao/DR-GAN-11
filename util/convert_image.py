ffa4af4fde66e637f3c43c4f8b6b915aba3e2e9c
jasonzhao <zzs3033@163.com>
2018-11-21 20:44:28 +0800

    etst

--- util/convert_image.py
+++ util/convert_image.py
@@ -9,31 +9,30 @@ def convert_image(data):
     if len(data.shape)==4:
         img = data.transpose(0, 2, 3, 1)
         # img = img / 2.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-        img = data * 255.
-=======
+
+        # img = img * 255.
+
         img = img * 255.
->>>>>>> parent of 676bb08... change image format
+
         # img = img[:,:,:,[2,1,0]]
-=======
+
         img = img * 255.
         img = img[:,:,:,[2,1,0]]
->>>>>>> parent of 6e1cd97... test
+
 
     else:
         img = data.transpose(1, 2, 0)
         # img = img / 2.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-        img = data * 255.
-=======
+
+
         img = img * 255.
->>>>>>> parent of 676bb08... change image format
+
+        # img = img * 255.
+
         # img = img[:,:,[2,1,0]]
-=======
-        img = img * 255.
+
+        # img = img * 255.
         img = img[:,:,[2,1,0]]
->>>>>>> parent of 6e1cd97... test
+
 
     return img.astype(np.uint8)
